3d77cf61e5db6034394467832f22c675baaa31a3
Spencer Russell <spencer.f.russell@gmail.com>
2017-10-12 16:54:00 -0400

    still in progress...

--- src/formats.jl
+++ src/formats.jl
@@ -1,13 +1,3 @@
-# used by WAVE_FORMAT_EXTENSIBLE
-struct WAVFormatExtension
-    nbits::UInt16 # overrides nbits in WAVFormat type
-    channel_mask::UInt32
-    sub_format::Array{UInt8, 1} # 16 byte GUID
-    
-    WAVFormatExtension() = new(0, 0, Array{UInt8, 1}(0))
-    WAVFormatExtension(nb, cm, sb) = new(nb, cm, sb)
-end
-
 # Required WAV Chunk; The format chunk describes how the waveform data is stored
 struct WAVFormat
     compression_code::UInt16
@@ -22,6 +12,27 @@ struct WAVFormat
     WAVFormat(cc, nchan, fs, bps, ba, nb, e) = new(cc, nchan, fs, bps, ba, nb, e)
 end
 
+# used by WAVE_FORMAT_EXTENSIBLE
+struct WAVFormatExtension
+    nbits::UInt16 # overrides nbits in WAVFormat type
+    channel_mask::UInt32
+    sub_format::Array{UInt8, 1} # 16 byte GUID
+
+    WAVFormatExtension() = new(0, 0, Array{UInt8, 1}(0))
+    WAVFormatExtension(nb, cm, sb) = new(nb, cm, sb)
+end
+
+function WAVFormatExtension(bytes)
+    if isempty(bytes)
+        return WAVFormatExtension()
+    end
+    # split bytes into valid_bits_per_sample, channel_mask, and sub_format
+    valid_bits_per_sample = (convert(UInt16, bytes[2]) << 8) | convert(UInt16, bytes[1])
+    channel_mask = (convert(UInt32, bytes[6]) << 24) | (convert(UInt32, bytes[5]) << 16) | (convert(UInt32, bytes[4]) << 8) | convert(UInt32, bytes[3])
+    sub_format = bytes[7:end]
+    return WAVFormatExtension(valid_bits_per_sample, channel_mask, sub_format)
+end
+
 const WAVE_FORMAT_PCM        = 0x0001 # PCM
 const WAVE_FORMAT_IEEE_FLOAT = 0x0003 # IEEE float
 const WAVE_FORMAT_ALAW       = 0x0006 # A-Law
@@ -30,6 +41,61 @@ const WAVE_FORMAT_EXTENSIBLE = 0xfffe # Extension!
 
 isextensible(fmt::WAVFormat) = (fmt.compression_code == WAVE_FORMAT_EXTENSIBLE)
 bits_per_sample(fmt::WAVFormat) = isextensible(fmt) ? fmt.ext.nbits : fmt.nbits
+
+function read_format(io::IO, chunk_size::UInt32)
+    # can I read in all of the fields at once?
+    orig_chunk_size = convert(Int, chunk_size)
+    if chunk_size < 16
+        error("The WAVE Format chunk must be at least 16 bytes")
+    end
+    const compression_code = read_le(io, UInt16)
+    const nchannels = read_le(io, UInt16)
+    const sample_rate = read_le(io, UInt32)
+    const bytes_per_second = read_le(io, UInt32)
+    const block_align = read_le(io, UInt16)
+    const nbits = read_le(io, UInt16)
+    ext = Array{UInt8, 1}(0)
+    chunk_size -= 16
+    if chunk_size > 0
+        const extra_bytes_length = read_le(io, UInt16)
+        if extra_bytes_length == 22
+            ext = read(io, UInt8, extra_bytes_length)
+        end
+    end
+    return WAVFormat(compression_code,
+                     nchannels,
+                     sample_rate,
+                     bytes_per_second,
+                     block_align,
+                     nbits,
+                     WAVFormatExtension(ext))
+end
+
+function write_format(io::IO, fmt::WAVFormat)
+    len = 16 # 16 is size of base format chunk
+    if isextensible(fmt)
+        len += 24 # 24 is the added length needed to encode the extension
+    end
+    # write the fmt subchunk header
+    write(io, b"fmt ")
+    write_le(io, convert(UInt32, len)) # subchunk length
+
+    write_le(io, fmt.compression_code) # audio format (UInt16)
+    write_le(io, fmt.nchannels) # number of channels (UInt16)
+    write_le(io, fmt.sample_rate) # sample rate (UInt32)
+    write_le(io, fmt.bytes_per_second) # byte rate (UInt32)
+    write_le(io, fmt.block_align) # byte align (UInt16)
+    write_le(io, fmt.nbits) # number of bits per sample (UInt16)
+
+    if isextensible(fmt)
+        write_le(io, convert(UInt16, 22))
+        write_le(io, fmt.ext.nbits)
+        write_le(io, fmt.ext.channel_mask)
+        @assert length(fmt.ext.sub_format) == 16
+        write(io, fmt.ext.sub_format)
+    end
+end
+
 
 # DEFINE_GUIDSTRUCT("00000001-0000-0010-8000-00aa00389b71", KSDATAFORMAT_SUBTYPE_PCM);
 const KSDATAFORMAT_SUBTYPE_PCM = [
@@ -81,15 +147,4 @@ function isformat(fmt::WAVFormat, code)
         return subtype == fmt.ext.sub_format
     end
     return fmt.compression_code == code
-end
-
-function WAVFormatExtension(bytes)
-    if isempty(bytes)
-        return WAVFormatExtension()
-    end
-    # split bytes into valid_bits_per_sample, channel_mask, and sub_format
-    valid_bits_per_sample = (convert(UInt16, bytes[2]) << 8) | convert(UInt16, bytes[1])
-    channel_mask = (convert(UInt32, bytes[6]) << 24) | (convert(UInt32, bytes[5]) << 16) | (convert(UInt32, bytes[4]) << 8) | convert(UInt32, bytes[3])
-    sub_format = bytes[7:end]
-    return WAVFormatExtension(valid_bits_per_sample, channel_mask, sub_format)
 end
